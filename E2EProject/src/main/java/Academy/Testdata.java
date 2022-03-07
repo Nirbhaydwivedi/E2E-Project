00db9fe571c363e3eef2c9bfd68d16edb54cfbcb
nirbhaydwivedi <nirbhaydwivedi@gmail.com>
2022-03-07 12:58:54 -0600

    E2E Project Commited

---
+++ E2EProject/src/main/java/Academy/Testdata.java
@@ -0,0 +1,16 @@
+package Academy;
+
+public class Testdata {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ E2EProject/src/test/java/Academy/Test12345.java
@@ -0,0 +1,10 @@
+package Academy;
+
+public class Test12345 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
